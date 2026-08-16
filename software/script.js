c1d09a26a61a9c307bdce92155d1447b9fccedea
Lyubelsky <lyubelsky.family@mail.ru>
2026-08-16 18:42:20 -0400

    Delete software directory

--- software/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.item').forEach(item => {
-    item.addEventListener('click', (event) => {
-        const link = document.createElement('a')
-        link.href = 'items/' + item.querySelector('h3').innerHTML.toLowerCase().replace(' ', '-') + '/index.html'
-        link.click()
-    })
-})
